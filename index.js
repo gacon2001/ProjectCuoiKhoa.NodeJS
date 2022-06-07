d0a9bcf023fb480fbfa5a0d7ee0f0fd24e357b3f
Misavan <kh.van0618@gmail.com>
2022-06-07 19:31:06 +0700

    graphpql

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { logger } = require('./src/middleware/logger');
 const path = require("path");//copy cùng __dirname -> require mọi thứ trên đầu
 
 const rootRouter = require('./src/routers'); 
+const { graphqlHTTP } = require('express-graphql');
+const graphqlSchema = require('./src/graphql/schema');
+const graphqlResolvers = require('./src/graphql/resolvers');
 
 const app = express();
 app.use(express.json());//destructuring -> undefined -> phải dùng express.json
@@ -16,6 +19,11 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 ///log tất cả những request gửi rới server của mình
 app.use(logger);
 
+// app.use("/graphql", graphqlHTTP({
+//     schema: graphqlSchema,
+//     rootValue: graphqlResolvers,
+//     graphiql: true, //giúp debug ở môi trg developer, ở mt production: false
+// })); //graphqlHTTP: confict sẵn express với graphql lại vs nhau, như 1 handler  
 app.use('/api',rootRouter);//tất cả api đều đi qua đường dẫn này mới vào rootRouter => p2: router của mìh (import vô)
 
 //check test connection
